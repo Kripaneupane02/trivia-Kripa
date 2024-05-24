c8a38b0fc58531c88b5f1da9b4926193e52c0416
kripaneupane02 <kripanepane96@gmail.com>
2024-05-24 17:35:30 +0545

    Question was set on javascript

---
+++ script.js
@@ -0,0 +1,30 @@
+const questions = [
+  {
+    question: "Which is the largest animal in the world?",
+    Answers: [
+      { text: "shark", correct: false },
+      { text: "Blue Whale", correct: true },
+      { text: "Elephant", correct: false },
+      { text: "Giraffe", correct: false },
+    ],
+  },
+  {
+    question: "What is the fastest land mammal?",
+    Answers: [
+      { text: "cheetah", correct: true },
+      { text: "Blue Whale", correct: false },
+      { text: "Leopard", correct: false },
+      { text: "Horse", correct: false },
+    ],
+  },
+
+  {
+    question: "How many legs do ant have?",
+    Answers: [
+      { text: "five", correct: false },
+      { text: "One", correct: false },
+      { text: "two", correct: false },
+      { text: "six", correct: true },
+    ],
+  },
+];
